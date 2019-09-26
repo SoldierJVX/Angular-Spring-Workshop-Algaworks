c79b0b4d27226c1c95c89bb76494ffbcb9c6207d
joaosilva <jv_rss@hotmail.com>
2019-09-26 14:22:24 -0300

    Implementação final workshop

--- comercial-ui/src/app/app.module.ts
+++ comercial-ui/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {TableModule} from 'primeng/table';
+import {PanelModule} from 'primeng/panel';
+import {InputTextModule, InputText} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 import { AppComponent } from './app.component';
 import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
@@ -19,9 +24,14 @@ import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao
     FormsModule, 
     HttpClientModule,
     BrowserAnimationsModule,
+    
     TableModule,
+    PanelModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
